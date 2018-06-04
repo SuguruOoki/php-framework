f655aaa466822b4f0ebdddae6c796304c1578bf7
SuguruOoki <suguru.oki.programming@gmail.com>
2018-06-04 23:51:50 +0900

    top画面のindexactionを実装

---
+++ web/app/controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once(dirname(__FILE__, 2) . '/lib/mytemplate/class/Mytemplate.class.php');
+
+class IndexController
+{
+    public function __construct() {
+        $this->mytemplate = new MyTemplate();
+    }
+    
+    public function indexAction() {
+        $this->mytemplate->setTemplate('top.php');
+        $this->mytemplate->assign('status', 'なにやら成功');
+        $this->mytemplate->display();
+    }
+}
