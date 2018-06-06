5b84c1f3214e323c3c4c1657c7538f9603afd568
SuguruOoki <suguru.oki.programming@gmail.com>
2018-06-06 21:00:35 +0900

    [修正] dispatcherを修正

--- web/app/lib/router/Dispatcher.php
+++ web/app/lib/router/Dispatcher.php
@@ -2,11 +2,31 @@
 class Dispatcher
 {
     private $system_root;
+    private $controller_level;
+    private $action_level;
     
-    public function setSystemRoot($path) {
+    private function setSystemRoot($path) {
         $this->system_root = rtrim($path, '/');
     }
     
+    /**
+     * dispatchを始めるディレクトリの階層を指定する
+     * 0はwebrootの階層を示す
+     *
+     * @param int $level Dispatchを始める階層。
+     * @return void
+     */
+    private function setFirstDispatchLevel($level = 0) {
+        $this->controller   = $level;
+        $this->action_level = $level + 1;
+    }
+    
+    /**
+     * dispatchを行う
+     * TODO: 現在は記事などのIDが入った時の処理を考慮していないため、入れたのちにはそこの処理を実装する
+     *
+     * @return void
+     */
     public function dispatch() {
         $request_uri = ltrim($_SERVER['REQUEST_URI'], '/');
         if ($request_uri === '/index.php' || $request_uri === '/' || $request_uri === '') {
@@ -15,17 +35,25 @@ class Dispatcher
         }
         
         $action_params = explode('/', $request_uri);
-        $controller = ucfirst($action_params[0]).'Controller';
-        $action = lcfirst(ucwords($action_params[1])).'Action';
+        $controller = ucfirst($action_params[$this->controller_level]).'Controller';
+        $action = lcfirst(ucwords($action_params[$this->action_level])).'Action';
         
         $this->run($controller, $action);
         
     }
     
-    public function run($class_name = 'TopController', $action_method = 'indexAction') {
-        require_once $this->system_root . '/controllers/' . $class_name . '.php';
+    /**
+     * actionメソッドを実行する。
+     * TODO: 記事の詳細を表示する場合などの考慮が足りないかも。
+     *
+     * @param  string $controller 実行するコントローラー
+     * @param  string $action     実行するアクションメソッド
+     * @return void
+     */
+    private function run($controller = 'TopController', $action = 'indexAction') {
+        require_once $this->system_root . '/controllers/' . $controller . '.php';
         
-        $controller_instance = new $class_name();    
+        $controller_instance = new $controller();
         $controller_instance->$action_method();
     }
 }
--- web/index.php
+++ web/index.php
@@ -3,4 +3,5 @@ require_once __DIR__ . '/app/lib/router/Dispatcher.php';
 
 $dispatcher = new Dispatcher();
 $dispatcher->setSystemRoot(__DIR__ . '/app');
+$dispatcher->setFirstDispatchLevel();
 $dispatcher->dispatch();
