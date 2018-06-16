e151f753bf6e7cb8fa392efcd45bc300bc0680c3
SuguruOoki <suguru.oki.programming@gmail.com>
2018-06-16 16:56:04 +0900

    [修正] basemodelの依存関係を修正

--- web/app/models/BaseModel.php
+++ web/app/models/BaseModel.php
@@ -1,6 +1,6 @@
 <?php 
 
-require_once(__DIR__ . '/../db/DbConnector.php')
+require_once(__DIR__ . '/../db/DbConnector.php');
 
 class BaseModel
 {
--- web/app/models/User.php
+++ web/app/models/User.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once(__DIR__ . '/BaseModel.php');
 
 class User extends BaseModel
 {
